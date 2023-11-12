004c5b36c418a64e55c3451d93cde618824d1306
nzamb <nzamb@umich.edu>
2023-11-11 23:28:55 -0500

    added draft of constraint generation

--- ori_ken/include/structure.hpp
+++ ori_ken/include/structure.hpp
@@ -1,12 +1,20 @@
+#include <iostream>
+#include <vector>
+
 #pragma once
 
-namespace Okin{
+namespace std{
     class structure{
 
         protected:
             char * name;
         private:
             size_t n_bodies;
+        public:
+            //generate constraints
+            void generateConstraints(vector<vector<double>> &nodes,
+                vector<vector<int>> &edges, size_t nnodes,size_t nedges,
+                vector<vector<int>> &const_nodes,vector<double> &C, vector<int> &colIdx, vector<int> &rowIdx);
 
     };
 }
