c7a611a5c897121e4372b8c34b9f80e044ee2281
jacobpavelka <jpavelka@umich.edu>
2023-12-04 10:12:17 -0500

    trying something

--- ori_kin/include/lapacke.h
+++ ori_kin/include/lapacke.h
@@ -2,5 +2,5 @@
 extern "C" {
     #define LAPACK_ROW_MAJOR               101
     #define LAPACK_COL_MAJOR               102
-    int LAPACKE_dgesdd(int, char,int,int,double*,int,double*,double*,int,double*,int);
+    int LAPACKE_dgesdd_(int, char,int,int,double*,int,double*,double*,int,double*,int);
 }
